57118c8a64ce03b73071e2e0b8cda6766da2fc48
AdamBurawski <a.burawski.post@gmail.com>
2023-09-01 10:35:05 +0200

    added error handling

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import cors from "cors";
 import "express-async-errors";
 import { log } from "console";
+import { ValidationError, handleError } from "./utlis/errors";
 
 const app = express();
 
@@ -11,6 +12,14 @@ app.use(
   })
 );
 app.use(json());
+
+//Routes...
+
+app.get("/", async (req, res) => {
+  throw new ValidationError("Ojejku!");
+});
+
+app.use(handleError);
 
 app.listen(3001, "0.0.0.0", () => {
   console.log("Listening on port http://localhost:3001");
---
+++ utlis/errors.ts
@@ -0,0 +1,20 @@
+import * as express from "express";
+import { NextFunction, Request, Response } from "express";
+
+export class ValidationError extends Error {}
+
+export const handleError = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: express.NextFunction
+): void => {
+  console.error(err);
+
+  res.status(err instanceof ValidationError ? 400 : 500).json({
+    message:
+      err instanceof ValidationError
+        ? err.message
+        : "Przepraszamy, spróbuj ponownie za kilka minut.",
+  });
+};
